98bd563dcda5ed6cb4b5f928da7908e0d91d63f5
majo48 <mart.jonasse@gmail.com>
2018-11-26 19:38:42 +0100

    Added GUI for /register The GUI queries the user for an email address. If given, the user is thanked. Both pages supply explanations and the Terms of Use (englisch).

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,24 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/register',
+    function (Request $request, Response $response, array $args) {
+        // Sample log message
+        $this->logger->info("/register");
+
+        $email = $request->getQueryParam('email');
+        if (!empty($email)){
+            return $this->renderer->render($response, 'thanks.phtml', $args);
+        }
+
+        // Render register view
+        return $this->renderer->render($response, 'register.phtml', $args);
+    });
