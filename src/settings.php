3a285bc167a8693c0ac3680df2e7efce75e71ba0
majo48 <mart.jonasse@gmail.com>
2019-06-13 13:11:13 +0200

    Added custom settings The application setting are defined in the file setting.php, using array variables, which are set using ansible playbooks and (where needed) the ansible vault for secret credentials.

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,19 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // Custom settings
+        // set values with ansible install(update)_playbook.yml
+        // get values with slim php $custom = $this->container->settings['custom'];
+        // or like $custom = $this->container->settings['custom']['mysqlDbName'];
+        'custom' => [
+            'mailUsername' => 'secret',
+            'mailPassword' => 'secret',
+            'mysqlDbName' => 'secret',
+            'mysqlDbRootPassword' => 'secret',
+            'mysqlDbUserName' => 'secret',
+            'mysqlDbUserPassword' => 'secret',
+        ],
     ],
 ];
+
