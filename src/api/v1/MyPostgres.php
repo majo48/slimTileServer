c6db0cf6b6e2aaff11c731faf0125336c616beab
majo48 <mart.jonasse@gmail.com>
2019-07-02 18:02:27 +0200

    Added code for converting to postgres Added code for transforming the OpenAddresses csv file to postgres database gis, table gwr. The conversion takes about 20 minutes.

--- src/api/v1/MyPostgres.php
+++ src/api/v1/MyPostgres.php
@@ -10,7 +10,8 @@ class MyPostgres
     use MyProjectDir{
         getProjectDir as protected;
     }
-    const CSVFILE = 'data/countrywide/ch/countrywide.csv';
+    // both files have an identical structure
+    const CSVFILECH= 'data/countrywide/ch/countrywide.csv';
 
     /** @var Container $container */
     protected   $container;
@@ -40,7 +41,7 @@ class MyPostgres
      */
     public function downloadCsv($country)
     {
-        $csv = $this->getProjectDir().'/'.self::CSVFILE;
+        $csv = $this->getProjectDir().'/'.self::CSVFILECH;
         //todo finish this
         return null;
     }
@@ -52,27 +53,29 @@ class MyPostgres
     public function updateDb($country)
     {
         // open csv file
-        $csv = $this->getProjectDir().'/'.self::CSVFILE;
+        $csv = $this->getProjectDir().'/'.self::CSVFILECH;
         $fn = fopen($csv, 'r');
         if ($fn===false){
             return 'Cannot open file '.$csv;
         }
 
         // read csv header
-        $header = explode(',', fgets($fn));
-        if ($fn===false){
-            return 'No header in file '.$csv;
+        $header = explode(',', rtrim(fgets($fn),"\r\n"));
+        if (($fn===false)||($this->checkHeader($header)===false)){
+            return 'No/wrong header in file '.$csv;
         }
         $this->clearDb();
 
         // read csv data
         $cntr = 0;
         while(!feof($fn)) {
-            $data = explode(',', fgets($fn));
+            $data = explode(',', rtrim(fgets($fn),"\r\n"));
             $this->addRow($header, $data);
             $cntr++;
         }
         fclose($fn);
+        $this->updateGwr();
+
         if ($cntr===0){
             return 'No data in file '.$csv;
         }
@@ -80,23 +83,109 @@ class MyPostgres
     }
 
     /**
-     * Clear rows from the postgres database
+     * Check the file header for the correct (expected) column index & names
+     * @param array $header
+     * @return boolean OK=true, error=false
      */
-    private function clearDb()
+    private function checkHeader($header)
     {
-        //todo finish this
-        $dummy = 'stop';
+        try{
+            if ($header[0]!=='LON') return false;
+            if ($header[1]!=='LAT') return false;
+            if ($header[2]!=='NUMBER') return false;
+            if ($header[3]!=='STREET') return false;
+            if ($header[5]!=='CITY') return false;
+            if ($header[7]!=='REGION') return false;
+            if ($header[8]!=='POSTCODE') return false;
+            if ($header[9]!=='ID') return false;
+            if ($header[10]!=='HASH') return false;
+            return true;
+        }
+        catch (\Exception $e){
+            return false;
+        }
     }
 
     /**
-     * Add one row to postgres database
+     * Clear all rows in postgres database gis, table gwr
+     * @return integer number of rows deleted
+     */
+    private function clearDb()
+    {
+        try{
+            $stmt = $this->pdoPostgres->prepare('DELETE FROM gwr');
+            $stmt->execute();
+            return $stmt->rowCount();
+        }
+        catch (Exception $e){
+            $this->container->logger->error(
+                "Database clear error: ".$e->getMessage()
+            );
+            return 0;
+        }
+    }
+
+    /**
+     * Add one row to postgres database gis, table gwr
      * @param array $header
      * @param array $data
+     * @return integer the id of the new record
      */
     private function addRow($header, $data)
     {
-        //todo finish this
-        $dummy = 'stop';
+        try{
+            // prepare statement for insert
+            $sql = 'INSERT INTO '.
+                'gwr(street,number,city,region,postcode,gwrId,hash,lat,lon) '.
+                'VALUES(:street,:number,:city,:region,:postcode,:gwrId,:hash,:lat,:lon)';
+            $stmt = $this->pdoPostgres->prepare($sql);
+
+            // pass values to the statement
+            $stmt->bindValue(':street', $data[3]);
+            $stmt->bindValue(':number', $data[2]);
+            $stmt->bindValue(':city', $data[5]);
+            $stmt->bindValue(':region', $data[7]);
+            $stmt->bindValue(':postcode', $data[8]);
+            $stmt->bindValue(':gwrId', $data[9]);
+            $stmt->bindValue(':hash', $data[10]);
+            $stmt->bindValue(':lat', $data[1]);
+            $stmt->bindValue(':lon', $data[0]);
+
+            // execute the insert statement
+            $stmt->execute();
+
+            // return generated id
+            return $this->pdoPostgres->lastInsertId('id');
+        }
+        catch (Exception $e){
+            $this->container->logger->error(
+                "Database insert error: ".$e->getMessage()
+            );
+            return 0;
+        }
+    }
+
+    /**
+     * Update all geometry fields in the postgres database
+     * @return integer number of rows updated
+     */
+    private function updateGwr()
+    {
+        try{
+            // prepare statement for update
+            $sql = "UPDATE gwr SET geom = ST_SetSRID(".
+                "ST_MakePoint(cast(LON AS float), cast(LAT AS float)), 4326);";
+            $stmt = $this->pdoPostgres->prepare($sql);
+            // execute the update statement
+            $stmt->execute();
+            return $stmt->rowCount();
+        }
+        catch (Exception $e){
+            $this->container->logger->error(
+                "Database update error: ".$e->getMessage()
+            );
+            return 0;
+        }
     }
 
 }
